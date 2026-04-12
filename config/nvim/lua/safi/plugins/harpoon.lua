cb15765621bc2a058425a1c1356c518af27ff94b
Safi Ullah <safiullah106@gamil.com>
2026-04-12 17:27:42 +0500

    made some keymap changes

--- config/nvim/lua/safi/plugins/harpoon.lua
+++ config/nvim/lua/safi/plugins/harpoon.lua
@@ -12,15 +12,13 @@ return {
         harpoon:setup()
         -- REQUIRED
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "ha", function() harpoon:list():add() end)
+        vim.keymap.set("n", "hx", function() harpoon:list():remove() end)
+        vim.keymap.set("n", "hv", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
-        vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+        for i = 1, 9 do
+            vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end, { noremap = true })
+        end
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
