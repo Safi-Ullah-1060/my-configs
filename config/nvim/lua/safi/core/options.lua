acf9e963cd6491e0a91eb7c9d728e56b516433e5
Safi Ullah <safiullah106@gamil.com>
2026-04-19 04:59:24 +0500

    added yazi functionality

--- config/nvim/lua/safi/core/options.lua
+++ config/nvim/lua/safi/core/options.lua
@@ -17,7 +17,9 @@ opt.wrap = true
 opt.ignorecase = true
 opt.smartcase = true
 
-opt.cursorline = true
+opt.cursorline = false
+vim.opt.guicursor =
+"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 opt.termguicolors = true
 opt.background = "dark"
